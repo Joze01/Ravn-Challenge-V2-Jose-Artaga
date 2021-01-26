1b881e1203dfd888d4c92621f8073a57f74050cc
Jose Arteaga <jarteaga@applaudostudios.com>
2021-01-26 12:23:52 -0600

    ✨ Add Empty Project

---
+++ app/src/main/java/com/applaudostudios/ravn_challenge_v2_jose_arteaga/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.applaudostudios.ravn_challenge_v2_jose_arteaga
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
